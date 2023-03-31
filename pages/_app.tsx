ebe20bad2bbc7f1879fc67723ffd6ffb421d0b30
NIBBA <m.sinakheiry2@gmail.com>
2023-03-31 23:15:46 +0330

    Modernext initialize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+//using next.js 13 font optimization to add our font to all pages
+import { Poppins } from "next/font/google";
+const font = Poppins({ subsets: ["latin"], weight: "400" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+		<main className={font.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
