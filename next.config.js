84f892a836da5beb0a3e0f306343c3c1e88be1fd
Sina Kheiry <m.sinakheiry.warwolf@gmail.com>
2023-10-06 23:04:20 +0330

    Updated project, Moved project to src directory

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-/**
- * @type {import('next').NextConfig}
- */
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const nextConfig = {};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,37 +11,37 @@ import Particle from "../components/Particle";
 export const font = localFont({
 	src: [
 		{
-			path: "../public/fonts/Mona-Sans-Light.woff2",
+			path: "../../public/fonts/Mona-Sans-Light.woff2",
 			weight: "300",
 			style: "normal",
 		},
 		{
-			path: "../public/fonts/Mona-Sans-Regular.woff2",
+			path: "../../public/fonts/Mona-Sans-Regular.woff2",
 			weight: "400",
 			style: "normal",
 		},
 		{
-			path: "../public/fonts/Mona-Sans-Medium.woff2",
+			path: "../../public/fonts/Mona-Sans-Medium.woff2",
 			weight: "500",
 			style: "normal",
 		},
 		{
-			path: "../public/fonts/Mona-Sans-SemiBold.woff2",
+			path: "../../public/fonts/Mona-Sans-SemiBold.woff2",
 			weight: "600",
 			style: "normal",
 		},
 		{
-			path: "../public/fonts/Mona-Sans-Bold.woff2",
+			path: "../../public/fonts/Mona-Sans-Bold.woff2",
 			weight: "700",
 			style: "normal",
 		},
 		{
-			path: "../public/fonts/Mona-Sans-ExtraBold.woff2",
+			path: "../../public/fonts/Mona-Sans-ExtraBold.woff2",
 			weight: "800",
 			style: "normal",
 		},
 		{
-			path: "../public/fonts/Mona-Sans-Black.woff2",
+			path: "../../public/fonts/Mona-Sans-Black.woff2",
 			weight: "900",
 			style: "normal",
 		},
