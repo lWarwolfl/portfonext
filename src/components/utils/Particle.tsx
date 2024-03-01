e31abe90b6ca48e09a246167fa4e31762bf4629c
Sina Kheiry <m.sinakheiry.warwolf@gmail.com>
2024-03-01 12:17:03 +0330

    Fixed particle issues by changing package

--- src/components/utils/Particle.tsx
+++ src/components/utils/Particle.tsx
@@ -1,83 +1,60 @@
 import styles from '@/styles/utils/Particle.module.scss'
-import { type ISourceOptions } from '@tsparticles/engine'
-import Particles, { initParticlesEngine } from '@tsparticles/react'
-import { loadSlim } from '@tsparticles/slim'
-import { useEffect, useMemo, useState } from 'react'
+import { useCallback } from 'react'
+import Particles from 'react-tsparticles'
+import type { Engine } from 'tsparticles-engine'
+import { loadSlim } from 'tsparticles-slim'
 
 export default function Particle() {
-   const [init, setInit] = useState(false)
-
-   useEffect(() => {
-      initParticlesEngine(async (engine) => {
-         await loadSlim(engine)
-      }).then(() => {
-         setInit(true)
-      })
+   const particlesInit = useCallback(async (engine: Engine) => {
+      await loadSlim(engine)
    }, [])
 
-   // const particlesLoaded = async (container?: Container): Promise<void> => {
-   //    console.log(container)
-   // }
-
-   const options: ISourceOptions = useMemo(
-      () => ({
-         fpsLimit: 60,
-         autoPlay: true,
-         background: {
-            color: {
-               value: 'transparent',
-            },
-         },
-         fullScreen: {
-            enable: false,
-         },
-         particles: {
-            color: {
-               value: ['#ffffff'],
-            },
-            collisions: {
-               enable: true,
-            },
-            move: {
-               direction: 'none',
-               enable: true,
-               outModes: {
-                  default: 'bounce',
+   return (
+      <Particles
+         className={styles.container}
+         id="tsparticles"
+         init={particlesInit}
+         options={{
+            fpsLimit: 120,
+            background: {
+               color: {
+                  value: 'transparent',
                },
-               random: false,
-               speed: 0.13,
-               straight: false,
             },
-            number: {
-               density: {
+            particles: {
+               color: {
+                  value: ['#ffffff'],
+               },
+               collisions: {
                   enable: true,
                },
-               value: 100,
+               move: {
+                  direction: 'none',
+                  enable: true,
+                  outModes: {
+                     default: 'bounce',
+                  },
+                  random: false,
+                  speed: 0.13,
+                  straight: false,
+               },
+               number: {
+                  density: {
+                     enable: true,
+                  },
+                  value: 100,
+               },
+               opacity: {
+                  value: 0.8,
+               },
+               shape: {
+                  type: 'circle',
+               },
+               size: {
+                  value: { min: 1.5, max: 2.5 },
+               },
             },
-            opacity: {
-               value: 0.8,
-            },
-            shape: {
-               type: 'circle',
-            },
-            size: {
-               value: { min: 1.5, max: 2.5 },
-            },
-         },
-      }),
-      []
+         }}
+      />
    )
-
-   if (init) {
-      return (
-         <Particles
-            className={styles.container}
-            id="tsparticles"
-            // particlesLoaded={particlesLoaded}
-            options={options}
-         />
-      )
-   }
-
-   return <></>
 }
