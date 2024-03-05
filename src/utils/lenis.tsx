966faadcd05052e178f63e8366ca33cb29a82bc3
Sina Kheiry <m.sinakheiry.warwolf@gmail.com>
2024-03-05 23:48:51 +0330

    Minor fixes

--- src/utils/lenis.tsx
+++ src/utils/lenis.tsx
@@ -15,7 +15,7 @@ export function LenisProvider(props: LenisProviderProps) {
   const [lenis, setLenis] = useState<Lenis | null>(null)
 
   useEffect(() => {
-    const lenis = new Lenis({ lerp: 0.07 })
+    const lenis = new Lenis({ lerp: 0.11 })
     setLenis(lenis)
 
     return () => {
