9dcc897607c4ef06429d874ca56ca6bc60dbbd28
Sina Kheiry <m.sinakheiry.warwolf@gmail.com>
2024-03-07 03:16:00 +0330

    Added text animation to hero

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,13 +1,45 @@
 import StyledButton from '@/components/utils/StyledButton'
 import styles from '@/styles/landing/Hero.module.scss'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { TextPlugin } from 'gsap/dist/TextPlugin'
+import { useRef } from 'react'
+
+if (typeof window !== 'undefined') {
+  gsap.registerPlugin(TextPlugin, useGSAP)
+}
 
 export default function Hero() {
-  return (
-    <div id="hero" className={styles.container}>
-      <div className={styles.intro}>
-        <div className={styles.big}>professional</div>
+  const main = useRef<HTMLDivElement>(null)
 
-        <div className={styles.small}>
+  useGSAP(
+    () => {
+      const tl = gsap.timeline({ repeat: -1, yoyo: true, repeatDelay: 1 })
+      tl.to('.text', {
+        duration: 2,
+        text: {
+          type: 'diff',
+          value: 'professional',
+          delimiter: '',
+        },
+      }).to('.text', {
+        duration: 1,
+        delay: 2,
+        text: { type: 'diff', value: 'proficient', delimiter: '' },
+      })
+    },
+    { scope: main }
+  )
+
+  return (
+    <div ref={main} id="hero" className={styles.container}>
+      <div className={styles.intro}>
+        <div className={styles.animatedtext}>
+          <div className={`text ${styles.text}`}>professional</div>
+          <span className={styles.caret}>|</span>
+        </div>
+
+        <div className={styles.description}>
           Front-end <span className={styles.accent}>Developer</span>{' '}
           <span className={styles.note}>(Next.js/React.js)</span>
         </div>
