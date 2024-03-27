889812df47ef6a63af52165e77f09c50ffcef7ae
Sina Kheiry <m.sinakheiry.warwolf@gmail.com>
2024-03-27 05:11:19 +0330

    Added layout to project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,119 +6,19 @@ import Languages from '@/components/landing/Languages'
 import Projects from '@/components/landing/Projects'
 import Skills from '@/components/landing/Skills'
 import Summary from '@/components/landing/Summary'
-import Footer from '@/components/layout/Footer'
-import Header from '@/components/layout/Header'
-import CustomHead from '@/components/utils/CustomHead'
-import useWindowSize from '@/lib/useWindowSize'
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
-import dynamic from 'next/dynamic'
-import { Poppins } from 'next/font/google'
-import { useRef } from 'react'
-
-const WebGLParticles = dynamic(
-  () => import('@/components/utils/Particles').then(({ WebGLParticles }) => WebGLParticles),
-  {
-    ssr: false,
-  }
-)
-
-const font = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-family',
-})
-
-if (typeof window !== 'undefined') {
-  gsap.registerPlugin(ScrollTrigger, useGSAP)
-}
+import MainLayout from '@/components/layout/MainLayout'
 
 export default function Home() {
-  const main = useRef<HTMLDivElement>(null)
-  const isMobile = useWindowSize()
-
-  useGSAP(
-    () => {
-      const titles = gsap.utils.toArray('.animated-title') as HTMLElement[]
-      const boxes = gsap.utils.toArray('.animated-container') as HTMLElement[]
-      const reverseboxes = gsap.utils.toArray('.animated-container-reverse') as HTMLElement[]
-
-      titles.forEach((box) => {
-        gsap.set(box, { x: -100, opacity: 0 })
-
-        gsap.to(box, {
-          x: 0,
-          opacity: 1,
-          scrollTrigger: {
-            trigger: box,
-            start: isMobile ? '300px bottom' : '100px bottom',
-            end: isMobile ? '650px bottom' : '450px bottom',
-            scrub: true,
-          },
-        })
-      })
-
-      boxes.forEach((box, index: number) => {
-        gsap.set(box, { x: -100, opacity: 0 })
-
-        gsap.to(box, {
-          x: 0,
-          opacity: 1,
-          scrollTrigger: {
-            trigger: box,
-            start: isMobile ? '350px bottom' : '150px bottom',
-            end:
-              boxes.length - 1 === index
-                ? 'bottom bottom'
-                : isMobile
-                  ? '700px bottom'
-                  : '500px bottom',
-            scrub: true,
-          },
-        })
-      })
-
-      reverseboxes.forEach((box, index: number) => {
-        gsap.set(box, { x: 100, opacity: 0 })
-
-        gsap.to(box, {
-          x: 0,
-          opacity: 1,
-          scrollTrigger: {
-            trigger: box,
-            start: isMobile ? '350px bottom' : '150px bottom',
-            end:
-              boxes.length - 1 === index
-                ? 'bottom bottom'
-                : isMobile
-                  ? '700px bottom'
-                  : '500px bottom',
-            scrub: true,
-          },
-        })
-      })
-    },
-    { scope: main }
-  )
-
   return (
-    <main className={font.className}>
-      <div id="full-size-image-slider"></div>
-      <CustomHead />
-      {!isMobile && <WebGLParticles />}
-      <Header />
-      <div className="width-fix" ref={main}>
-        <Hero />
-        <Summary />
-        <Skills />
-        <Experiences />
-        <Projects />
-        <Educations />
-        <Languages />
-        <Footer />
-        <Images />
-      </div>
-    </main>
+    <MainLayout>
+      <Hero />
+      <Summary />
+      <Skills />
+      <Experiences />
+      <Projects />
+      <Educations />
+      <Languages />
+      <Images />
+    </MainLayout>
   )
 }
