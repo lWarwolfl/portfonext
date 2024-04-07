ca36cfd0850ef89ef81113cbef3baa08b35291f9
Sina Kheiry <m.sinakheiry.warwolf@gmail.com>
2024-04-07 16:35:45 +0330

    Updated title and description

--- src/components/utils/CustomHead.tsx
+++ src/components/utils/CustomHead.tsx
@@ -15,9 +15,9 @@ export default function CustomHead(props: CustomHeadProps) {
   const router = useRouter()
 
   const seo = {
-    title: 'Sina Kheiri - Front-end developer(Next.js/React.js)',
+    title: 'Sina Kheiri - Front-end Developer | Next.js/React.js | Typescript | Web3',
     description:
-      'As a Front-end developer with 4 years of experience, I possess a diverse skill set in my field of work. With a strong focus on delivering clean code, high-quality projects and exceptional user interfaces, I am committed to staying up-to-date with the latest industry trends and best practices.',
+      'As a Front-end  developer with 4 years of experience, I possess a diverse skill set in my field of work. With a strong focus on delivering clean code, high-quality projects and exceptional user interfaces, I am committed to staying up-to-date with the latest industry trends and best practices.',
     url: router.asPath,
     image: preview.src,
     twitterCardType: 'summary',
